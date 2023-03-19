820b67c47b602caefdba9fa6ccbd8281763477a6
Sam516-Dev <samuelnjoroge3455@gmail.com>
2023-03-20 00:07:51 +0300

    changes
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css'
 function App() {
   return (
     <fragment>
-      <div className=" bg-red-500 mx-10 flex">
-        <div className=" mx-auto mt-10 rounded-full py-7 justify-between flex bg-blue-500 font-medium">
+      <div className=" justify-between py-4 mt-8 items-center bg-red-500 mx-10 flex">
+        <div className=" flex-col gap-5 px-10  mx-auto mt-10 rounded-full py-7 justify-between flex bg-blue-500 font-medium">
           <h1 className=" text-white"> Hey there Sam </h1>
           <h1 className=" text-white"> Hey there Sam </h1>
         </div>
-        <div className="mx-auto bg-green-500  mt-10 rounded-full  py-7 justify-between flex font-medium">
+        <div className=" flex-col gap-5 px-10 mx-auto bg-green-500  mt-10 rounded-full  py-7 justify-between flex font-medium">
+          <h1 className=" text-white"> Hey there Sam </h1>
+          <h1 className=" text-white"> Hey there Sam </h1>
+        </div>
+        <div className=" flex-col gap-5 px-10  mx-auto mt-10 rounded-full py-7 justify-between flex bg-blue-500 font-medium">
+          <h1 className=" text-white"> Hey there Sam </h1>
+          <h1 className=" text-white"> Hey there Sam </h1>
+        </div>
+        <div className=" flex-col gap-5 px-10 mx-auto bg-green-500  mt-10 rounded-full  py-7 justify-between flex font-medium">
           <h1 className=" text-white"> Hey there Sam </h1>
           <h1 className=" text-white"> Hey there Sam </h1>
         </div>
