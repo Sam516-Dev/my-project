d498d1ac7f961973bb45250897bdbb9bb176eff1
Sam516-Dev <samuelnjoroge3455@gmail.com>
2023-03-20 00:00:38 +0300

    comments
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className=" font-medium items-center">
-      <h1> Hey there Sam </h1>
-    </div>
-  );
+    <fragment>
+      <div className=" bg-red-500 mx-10 flex">
+        <div className=" mx-auto mt-10 rounded-full py-7 justify-between flex bg-blue-500 font-medium">
+          <h1 className=" text-white"> Hey there Sam </h1>
+          <h1 className=" text-white"> Hey there Sam </h1>
+        </div>
+        <div className="mx-auto bg-green-500  mt-10 rounded-full  py-7 justify-between flex font-medium">
+          <h1 className=" text-white"> Hey there Sam </h1>
+          <h1 className=" text-white"> Hey there Sam </h1>
+        </div>
+      </div>
+    </fragment>
+  )
 }
 
-export default App;
+export default App
