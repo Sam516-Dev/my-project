02d0ddec15ff05a63a9e254b8f807da10923c6a7
Sam516-Dev <samuelnjoroge3455@gmail.com>
2023-03-22 23:46:35 +0300

    Navbar component
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <fragment>
-      <div className=" justify-between py-4 mt-8 items-center bg-red-500 mx-10 flex">
-        <div className=" flex-col gap-5 px-10  mx-auto mt-10 rounded-full py-7 justify-between flex bg-blue-500 font-medium">
-          <h1 className=" text-white"> Hey there Sam </h1>
-          <h1 className=" text-white"> Hey there Sam </h1>
-        </div>
-        <div className=" flex-col gap-5 px-10 mx-auto bg-green-500  mt-10 rounded-full  py-7 justify-between flex font-medium">
-          <h1 className=" text-white"> Hey there Sam </h1>
-          <h1 className=" text-white"> Hey there Sam </h1>
-        </div>
-        <div className=" flex-col gap-5 px-10  mx-auto mt-10 rounded-full py-7 justify-between flex bg-blue-500 font-medium">
-          <h1 className=" text-white"> Hey there Sam </h1>
-          <h1 className=" text-white"> Hey there Sam </h1>
-        </div>
-        <div className=" flex-col gap-5 px-10 mx-auto bg-green-500  mt-10 rounded-full  py-7 justify-between flex font-medium">
-          <h1 className=" text-white"> Hey there Sam </h1>
-          <h1 className=" text-white"> Hey there Sam </h1>
-        </div>
+      <div>
+        <Navbar/>
       </div>
     </fragment>
   )
