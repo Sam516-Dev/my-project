558b37c876fa43b156bc8dcc46d4be2e35d60d65
Sam516-Dev <samuelnjoroge3455@gmail.com>
2023-03-24 01:49:22 +0300

    done
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/components/FrontBar.js
+++ src/components/FrontBar.js
@@ -4,28 +4,58 @@ function FrontBar() {
   return (
     <div>
       <div>
-        <div className=" bg-green-400 py-20 rounded-lg mt-10">
-          <div className=" bg-yellow-300 px-14 flex items-center gap-7">
-            <div className="gap-2 flex items-center">
+        <div className=" bg-blue-500 py-32 rounded-lg mt-10">
+          <div className=" px-14 flex -mt-28 items-center gap-7">
+            <div className="gap-1 text-white font-semibold text-lg flex items-center">
               <input
                 type="radio"
                 className="form-radio text-indigo-600 h-5 w-5"
               />
               <label> Round Trip</label>
             </div>
-            <div className=" flex items-center gap-2">
+            <div className=" text-white font-semibold text-lg flex items-center gap-1">
               <input
                 type="radio"
                 className="form-radio text-indigo-600 h-5 w-5"
               />
               <label> Round Trip</label>
             </div>
-            <div className=" flex items-center gap-2">
+            <div className=" text-white font-semibold text-lg flex items-center gap-1">
               <input
                 type="radio"
                 className="form-radio text-indigo-600 h-5 w-5"
               />
               <label> Round Trip</label>
+            </div>
+          </div>
+
+          <div className=" px-14 flex items-center gap-7">
+            <div className="gap-1 text-white font-semibold text-lg flex items-center">
+              <label> Origin City </label>
+              <input
+                type="text"
+                className="form-radio text-indigo-600 h-5 w-5"
+              />
+            </div>
+            <div className=" text-white font-semibold text-lg flex items-center gap-1">
+              <label> Destination </label>
+              <input
+                type="text"
+                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username"/>
+            </div>
+            <div className=" text-white font-semibold text-lg flex items-center gap-1">
+              <label> Departure/Return </label>
+              <input
+                type="text"
+                className="form-radio text-indigo-600 h-5 w-5"
+              />
+            </div>
+            <div className=" text-white font-semibold text-lg flex items-center gap-1">
+              <label> Travellers </label>
+              <input
+                type="text"
+                className="form-radio text-indigo-600 h-5 w-5"
+              />
             </div>
           </div>
         </div>
