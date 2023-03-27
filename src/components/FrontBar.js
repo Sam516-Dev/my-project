787a74eb58e32d7ad6bfb17b25c3ac6eab22d9fa
Sam516-Dev <samuelnjoroge3455@gmail.com>
2023-03-27 22:12:06 +0300

    media
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/components/FrontBar.js
+++ src/components/FrontBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+
+
 function FrontBar() {
   return (
     <div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import logo from '../Media/Assets/logo.svg'
+import Bg from '../Media/Assets/Bg.jpg'
+
+
+
 
 export default function () {
   return (
     <div className=" border-b border-blue-500 py-4 flex justify-between">
+      <div>
+      <svg className=' h-10 w-10 text-red-600 bg-yellow-300'>
+        <use xlinkHref={logo}></use>
+      </svg>
+      </div>
       <div >
         <ul className=" text-blue-900 gap-8 font-medium text-lg flex">
           <li>
