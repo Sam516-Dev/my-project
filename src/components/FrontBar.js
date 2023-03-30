e77d15838384381ae2fe72f8c27898a601f44966
Sam516-Dev <samuelnjoroge3455@gmail.com>
2023-03-30 23:57:28 +0300

    refactor
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/components/FrontBar.js
+++ src/components/FrontBar.js
@@ -86,36 +86,12 @@ function FrontBar() {
         this is the 4th wrapped div and should inlude outer elements of the web
         page
       </div>
-      <div>
-        this is the 4th wrapped div and should inlude outer elements of the web
-        page
-      </div><div>
-      this is the 4th wrapped div and should inlude outer elements of the web
-      page
-    </div><div>
+     <div>
     this is the 4th wrapped div and should inlude outer elements of the web
     page
   </div><div>
   this is the 4th wrapped div and should inlude outer elements of the web
   page
-</div><div>
-this is the 4th wrapped div and should inlude outer elements of the web
-page
-</div><div>
-this is the 4th wrapped div and should inlude outer elements of the web
-page
-</div><div>
-this is the 4th wrapped div and should inlude outer elements of the web
-page
-</div><div>
-this is the 4th wrapped div and should inlude outer elements of the web
-page
-</div><div>
-this is the 4th wrapped div and should inlude outer elements of the web
-page
-</div><div>
-this is the 4th wrapped div and should inlude outer elements of the web
-page
 </div><div>
 this is the 4th wrapped div and should inlude outer elements of the web
 page
