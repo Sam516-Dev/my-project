bf8a7852ed9acf78851207576fb724d06c6ce4a8
Sam516-Dev <samuelnjoroge3455@gmail.com>
2023-04-04 23:51:24 +0300

    Gallery component
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css'
 import Content from './components/Content'
 import FrontBar from './components/FrontBar'
+import { Gallery } from './components/Gallery'
 import Navbar from './components/Navbar'
 import Bg from './Media/Assets/Bg.jpg'
+
 
 function App() {
   return (
     <div className='bg-my-bg-color min-h-screen'>
       <img src={Bg} alt="bg" className="absolute object-cover w-full h-[25rem]" />
-      <div className="relative mx-20">
+      <div className="relative mx-20 pb-32">
         <Navbar />
         <FrontBar />
         <Content />
+        <Gallery  />
+
       </div>
     </div>
   )
